456cb7e7a51d2611c2de227957def6335398f9ee
Caligulaph <qm25qm@gmail.com>
2008-11-11 14:04:39 -0500

    (no commit message)

--- src/Cupple.java
+++ src/Cupple.java
@@ -1,16 +1,24 @@
-public class Cupple
+public class Cupple extends Character
 {
 	private Student male;
 	private Student female;
 	
-	private int hp;
-	private double speed;
+	private int x;
+	private int y;
+	
+	public Cupple(int x,int y,int hp,int maxHp,double speed)
+	{
+		super(hp,maxHp,speed);
+		this.x = x;
+		this.y = y;
+	}
 	
 	public Cupple(Student a,Student b)
 	{
+		super();
 		if(a.getGender() == 'm')
 		{
-			male = a;
+			male = new Student(x,y,a.getHp(),a.getMaxHp(),a.getSpeed(),'m');
 			female = b;
 		}
 		else
@@ -18,7 +26,17 @@ public class Cupple
 			male = b;
 			female = a;
 		}
-		hp = a.getHealth()*b.getHealth()
+		hp = a.getHp()*b.getHp();
 		speed = (a.getSpeed()+b.getSpeed)/2;
+	}
+	
+	public Student getMale()
+	{
+		return male;
+	}
+	
+	public Student getFemale()
+	{
+		return female;
 	}
 }
