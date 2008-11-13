ec515d2a38377eb59d96f75c1cbe787203577c03
varq <jason.khamphila@gmail.com>
2008-11-13 15:41:03 -0500

    Updated Staff class. Added Stark class for testing.

---
+++ src/Stark.java
@@ -0,0 +1,25 @@
+/**
+ * Park View Protractor
+ * 
+ * @author Jason of Javateerz
+ *
+ * The jawesomeest charater in the hole game
+ *
+ */
+
+public class Stark extends Staff
+{
+	public Stark(int x, int y, int hp, int maxHp, double speed, int tp, int maxTp, int damage)
+	{
+		super(x, y, hp, maxHp, speed, tp, maxTp, damage);
+	}
+	
+	public void attack()
+	{
+		//AOE aoe=new AOE
+	}
+	public void doSkill()
+	{
+		
+	}
+}
