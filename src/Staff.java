a81073856c270b0c1d54507f5d781fb0f2bd16a3
varq <jason.khamphila@gmail.com>
2008-11-13 13:29:07 -0500

    Updated Staff class again (again). -_-

--- src/Staff.java
+++ src/Staff.java
@@ -1,20 +1,39 @@
 /**
  * Park View Protector
  * 
- * @author Javateerz
+ * @author Jason of Javateerz
  *
  * The player class.
  *
+ * Update log
+ * -Updated parameters. (TP=Teacher Points)
+ * -Added doSkill. Unsure what this is gonna' do yet.
+ * -Added useItem.
  */
 
-public class Staff extends Entity
+public abstract class Staff extends Character
 {
-	/*
-	 * Creates a Staff member.
-	 * 
-	 */
-	public Staff(int health, double speed)
+	private int tp;
+	private int maxTp;
+	
+	// Creates a Staff member.
+	public Staff(int x, int y, int hp, int maxHp, double speed, int tp, int maxTp)
 	{
-		super(health, speed);
+		super(x, y, hp, maxHp, speed);
+		this.tp=tp;
+		this.maxTp=maxTp;
+	}
+	
+	public void doSkill()
+	{
+		//TO-DO: No idea what to so far!
+	}
+	
+	public void useItem(int item)
+	{
+		/*if(inventory.get(item) instanceof KeyItem)
+		{
+			inventory.get(item).run();
+		}*/
 	}
 }
