722ce1d98558b4816ae89b13376af8dfd9109a83
varq <jason.khamphila@gmail.com>
2009-01-13 14:14:22 -0500

    Attack has been updated to not work!

--- src/Attack.java
+++ src/Attack.java
@@ -11,13 +11,9 @@ import java.awt.Rectangle;
 
 public class Attack extends Movable
 {
-	private int x;
-	private int y;
 	private double speed;
 	private String name;
-	private int xSize;
-	private int ySize;	
-	private int damage;
+	private int x, y, xSize, ySize, damage, duration, time=0;
 	//Target: True=Student, FLASE=Staff
 	private boolean isStudent;
 	/*
@@ -28,45 +24,73 @@ public class Attack extends Movable
 	 */
 	private int type;
 	
-	public Attack(int x, int y, double speed, String name, int direct, int damage, boolean isStudent, int type)
+	public Attack(int x, int y, double speed, String name, int direct, int damage, int duration,
+			boolean isStudent, int type)
 	{
-		super(x, y, speed);
+		super(x+20, y+32, speed);
+		this.type=type;
 		this.name=name;
 		this.direction=direct;
 		this.damage=damage;
 		this.isStudent=isStudent;
-		this.type=type;
+		this.duration=duration;
+		switchXY();
 	}
 	
-	/*public ArrayList detect()
+	public int getDuration()
 	{
-		ArrayList targets;
-		switch(type)
-		{
-			case Type.FRONT:
-				break;
-		}
-		return targets;
-	}*/
+		return duration;
+	}
+	
+	public int getTime()
+	{
+		return time;
+	}
+	
+	public void tick()
+	{
+		time++;
+	}
 	
 	public void switchXY()
 	{
-		int temp;
 		if(direction==Direction.NORTH)
 		{
 			this.sprite		= DataStore.INSTANCE.getSprite("images/"+name+"_n.png");
+			System.out.println(this.getDirection());
+			//this.move(-this.getBounds().width/4,-this.getBounds().height/4);
+			this.x+=(int) Math.round(-this.getBounds().width/4 * speed);
+			this.y+=(int) Math.round(-this.getBounds().height/4 * speed);
 		}
 		else if(direction==Direction.SOUTH)
 		{
 			this.sprite		= DataStore.INSTANCE.getSprite("images/"+name+"_s.png");
+			System.out.println(this.getDirection());
+			//this.move(-this.getBounds().width/4,-this.getBounds().height/4);
+			this.x+=(int) Math.round(-this.getBounds().width/4 * speed);
+			this.y+=(int) Math.round(-this.getBounds().height/4 * speed);
 		}
 		else if(direction==Direction.WEST)
 		{
 			this.sprite		= DataStore.INSTANCE.getSprite("images/"+name+"_w.png");
+			System.out.println(this.getDirection());
+			//this.move(-this.getBounds().width/4,-this.getBounds().height/4);
+			this.x+=(int) Math.round(-this.getBounds().width/4 * speed);
+			this.y+=(int) Math.round(-this.getBounds().height/4 * speed);
 		}
 		else /*Implied else if for EAST*/
 		{
 			this.sprite		= DataStore.INSTANCE.getSprite("images/"+name+"_e.png");
+			System.out.println(this.getDirection());
+			//this.move(-this.getBounds().width/4,-this.getBounds().height/4);
+			this.x+=(int) Math.round(-this.getBounds().width/4 * speed);
+			this.y+=(int) Math.round(-this.getBounds().height/4 * speed);
+		}
+		
+		if(type==Type.FRONT)
+		{
+			System.out.println(this.getDirection());
+			this.move(15);
 		}
 	}
 }
