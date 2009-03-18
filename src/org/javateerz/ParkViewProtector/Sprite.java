4029350eead5fcefd725ce19eeeaf153a2b8e544
mutantmonkey <mutantmonkey@gmail.com>
2009-03-18 18:11:04 -0400

    Almost got OpenGL working :D

--- src/org/javateerz/ParkViewProtector/Sprite.java
+++ src/org/javateerz/ParkViewProtector/Sprite.java
@@ -6,20 +6,26 @@
 
 package org.javateerz.ParkViewProtector;
 
-import java.awt.*;
+import org.lwjgl.opengl.GL11;
 
 public class Sprite
 {
 	private GLTexture texture;
+	
+	private int width;
+	private int height;
 	
 	/**
 	 * Create a new sprite
 	 * 
 	 * @param image An image to use as the sprite
 	 */
-	public Sprite(Image img)
+	public Sprite(GLTexture texture)
 	{
-		this.img		= img;
+		texture			= texture;
+		
+		width			= texture.getImageWidth();
+		height			= texture.getImageHeight();
 	}
 	
 	/**
@@ -29,7 +35,7 @@ public class Sprite
 	 */
 	public int getWidth()
 	{
-		return img.getWidth(null);
+		return width;
 	}
 	
 	/**
@@ -39,7 +45,7 @@ public class Sprite
 	 */
 	public int getHeight()
 	{
-		return img.getHeight(null);
+		return height;
 	}
 	
 	/**
@@ -51,6 +57,28 @@ public class Sprite
 	 */
 	public void draw(int x, int y)
 	{
-		//g.drawImage(img, x, y, null);
+		GL11.glPushMatrix();
+		
+		texture.bind();
+		
+		// move to selected location
+		GL11.glTranslatef(x, y, 0);
+		
+		// set color for drawing
+		GL11.glColor3f(1, 1, 1);
+		
+		// draw a quadrilateral
+		GL11.glBegin(GL11.GL_QUADS);
+		GL11.glTexCoord2f(0, 0);
+		GL11.glVertex2f(0, 0);
+		GL11.glTexCoord2f(0, texture.getHeight());
+		GL11.glVertex2f(0, height);
+		GL11.glTexCoord2f(texture.getWidth(), texture.getHeight());
+		GL11.glVertex2f(width, height);
+		GL11.glTexCoord2f(texture.getWidth(), 0);
+		GL11.glVertex2f(width, 0);
+		GL11.glEnd();
+		
+		GL11.glPopMatrix();
 	}
 }
