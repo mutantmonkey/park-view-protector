f922b46be0697ac774592885afdd4f294420382e
mutantmonkey <mutantmonkey@gmail.com>
2009-05-02 14:52:16 -0400

    Updated logo and added charater selection graphic

--- src/org/javateerz/ParkViewProtector/CharSelect.java
+++ src/org/javateerz/ParkViewProtector/CharSelect.java
@@ -7,12 +7,12 @@ import org.newdawn.slick.TrueTypeFont;
 
 public class CharSelect extends GameScreen implements KeyListener
 {
-	public static final int CHAR_X		= 50;
-	public static final int CHAR_Y		= 50;
+	public static final int CHAR_X		= 150;
+	public static final int CHAR_Y		= 200;
 	
-	public static final int NAME_X		= CHAR_X + 100;
-	public static final int NAME_Y		= CHAR_Y;
+	public static final int NAME_PAD_X	= 20;
 	
+	private Sprite bgImage;
 	private Font nameFont;
 	
 	private Staff[] choices;
@@ -21,6 +21,8 @@ public class CharSelect extends GameScreen implements KeyListener
 	public CharSelect(ParkViewProtector p)
 	{
 		driver							= p;
+		
+		bgImage							= DataStore.INSTANCE.getSprite("char_select.png");
 		
 		nameFont						= new TrueTypeFont(new java.awt.Font(
 				"System", java.awt.Font.PLAIN, 32), true);
@@ -74,14 +76,21 @@ public class CharSelect extends GameScreen implements KeyListener
 		addKeyListener(this);
 		poll();
 		
+		bgImage.draw();
+		
 		Staff selectedChar				= choices[selectedIndex];
 		
 		selectedChar.draw();
 		
-		GLString name					= new GLString(selectedChar.getName(), NAME_X,
-				NAME_Y);
+		int x							= (int) (selectedChar.getBounds().getX() +
+			selectedChar.getBounds().getWidth() + NAME_PAD_X);
+		
+		int y							= (int) (selectedChar.getBounds().getY() +
+			(selectedChar.getBounds().getHeight() / 4));
+		
+		GLString name					= new GLString(selectedChar.getName(), x, y);
 		name.setFont(nameFont);
-		name.setColor(ParkViewProtector.COLOR_TEXT_2);
+		name.setColor(ParkViewProtector.COLOR_TEXT_1);
 		name.draw();
 	}
 	
