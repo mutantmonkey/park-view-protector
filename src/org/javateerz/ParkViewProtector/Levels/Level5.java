7933329e6e62b9be47b045c91358a1d56b7d239b
mutantmonkey <mutantmonkey@gmail.com>
2009-05-21 15:32:52 -0400

    New sound effect for male band students courtesy of Paul

---
+++ src/org/javateerz/ParkViewProtector/Levels/Level5.java
@@ -0,0 +1,81 @@
+package org.javateerz.ParkViewProtector.Levels;
+
+import java.util.ArrayList;
+
+import org.javateerz.ParkViewProtector.Game;
+import org.javateerz.ParkViewProtector.ParkViewProtector;
+import org.javateerz.ParkViewProtector.Wall;
+import org.javateerz.ParkViewProtector.Students.Student;
+
+public class Level5 implements Level
+{
+	public static final int MIN_STUDENTS		= 4;
+	public static final int MAX_STUDENTS		= 10;
+	public static final int MAX_STUDENT_SPEED	= 1;
+	public static final double GENDER_CHANCE	= 0.5;
+	
+	private Game game;
+	
+	public Level5(Game g)
+	{
+		game									= g;
+	}
+	
+	public String getBG()
+	{
+		return "terrazzo_green.png";
+	}
+	
+	public String getMusic()
+	{
+		return "heavyset.ogg";
+	}
+	
+	public ArrayList<Student> getStudents()
+	{
+		ArrayList<Student> students	= new ArrayList<Student>();
+		
+		// create a random number of students using MIN_STUDENTS and MAX_STUDENTS; multiply
+		// it by 2 and divide to ensure that an even number is created to ensure proper
+		// coupling
+		int numStudents				= (int) (Math.random() * (MAX_STUDENTS - MIN_STUDENTS + 1)) + MIN_STUDENTS;
+		numStudents					= Math.round(numStudents * 2 / 2);
+		
+		Student student				= null;
+		
+		int x, y, maxHp;
+		double speed;
+		char gender;
+		
+		for(int i = 0; i < numStudents; i++)
+		{
+			x						= (int) (Math.random() * ParkViewProtector.WIDTH);
+			y						= (int) (Math.random() * ParkViewProtector.HEIGHT);
+			speed					= Math.random() * MAX_STUDENT_SPEED + 1;
+			gender					= (Math.random() <= GENDER_CHANCE) ? 'm' : 'f';
+			maxHp					= (int)(Math.random()*30);
+			
+			student					= new Student(game, x, y, maxHp, speed, gender, Student.BAND);
+			
+			// make sure that the student is not spawned on top of a wall)
+			while(!student.canMove(student.getBounds()))
+			{
+				x					= (int) (Math.random() * ParkViewProtector.WIDTH) + 1;
+				y					= (int) (Math.random() * ParkViewProtector.HEIGHT) + 1;
+				
+				student.moveTo(x, y);
+			}
+			
+			student.setAggro(true);
+			
+			students.add(student);
+		}
+		
+		return students;
+	}
+	
+	public ArrayList<Wall> getWalls()
+	{
+		return new ArrayList<Wall>();
+	}
+}
